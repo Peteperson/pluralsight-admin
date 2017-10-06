ff3fab0222a4c24c7f6469d4f198ef66e2943fab
peteperson <Peteperson_2000@yahoo.com>
2017-10-06 22:02:25 +0300

    Initial commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+
+class HomePage extends React.Component {
+  render(){
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Admin</h1>
+        <p>React, Redux and react router in ES6 for ultra responsive web apps</p>
+        <Link to="about" className="btn btn-primary btn-lg">Fuck you more</Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
