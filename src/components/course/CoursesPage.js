a0dc838e1aafbc250a25a239b38084252063be54
peteperson <Peteperson_2000@yahoo.com>
2017-10-08 23:15:57 +0300

    Actions and Reducers

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,44 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 
 class CoursesPage extends React.Component {
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      course: { title: "" }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const crs = this.state.course;
+    crs.title = event.target.value;
+    this.setState({course: crs});
+  }
+
+  onClickSave(){
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add course</h2>
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
+        <input type="submit" onClick={this.onClickSave} value="save" />
       </div>
     );
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps){
+  //state.courses equals courses in line 2 of ...reducers/index.js
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
