06702849da3037f24f1e45c15d58f484c719de11
Panos Argoudelis <pargoudelis@eurobank.gr>
2017-10-25 18:11:28 +0300

    add saga for fetching users

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,7 +4,8 @@ import initialState from './initialState';
 export default function userReducer(state = initialState.users, action) {
   switch (action.type) {
     case types.GET_USER_LIST_SUCCESS:
-      return action.users;
+      console.log(action.userList);
+      return action.userList;
     default:
       return state;
   }
--- src/sagas/getUsersSaga.js
+++ src/sagas/getUsersSaga.js
@@ -1,10 +1,11 @@
 import {take, put, call, apply} from 'redux-saga/effects';
 import fetch from 'isomorphic-fetch';
-import {GET_USER_LIST} from '../actions/actionTypes';
+import {GET_USER_LIST, GET_USER_LIST_SUCCESS} from '../actions/actionTypes';
+import {usersRecieved} from '../actions/userActions';
 
 export function* getUsersSaga(){
-  console.log("into the generator");
   const qwe = yield take(GET_USER_LIST);
   const response = yield call(fetch, `http://jsonplaceholder.typicode.com/users`);
   const userList = yield apply(response, response.json);
+  yield put(usersRecieved(userList));
 }
