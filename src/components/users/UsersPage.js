52840d5658805808d9d38661f411a3f9b7bf29b2
peteperson <peteperson_2000@yahoo.com>
2017-10-25 23:48:48 +0300

    Show users page

---
+++ src/components/users/UsersPage.js
@@ -0,0 +1,58 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as userActions from '../../actions/userActions';
+import UserList from './UserList';
+import {browserHistory} from 'react-router';
+
+class UsersPage extends React.Component {
+  constructor(props, context){
+    super(props, context);
+    this.redirectToAddUserPage = this.redirectToAddUserPage.bind(this);
+  }
+
+  userRow(user, index){
+    return (
+      <div key={index}>{user.title}</div>
+      );
+  }
+
+  redirectToAddUserPage(){
+    browserHistory.push('/user');
+  }
+
+  render() {
+    const {users} = this.props;
+
+    return (
+      <div>
+        <h1>Users</h1>
+        <input type="submit"
+              value="Add user"
+              className="btn btn-primary"
+              onClick={this.redirectToAddUserPage}/>
+        {<UserList users={users} />}
+      </div>
+    );
+  }
+}
+
+UsersPage.propTypes = {
+  users: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+  //state.users equals users in line 2 of ...reducers/index.js
+  return {
+    users: state.users
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+      actions: bindActionCreators(userActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersPage);
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import UsersPage from './components/users/UsersPage';
 import ManageCoursePage from './components/course/ManageCoursePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="users" component={UsersPage} />
     <Route path="course" component={ManageCoursePage} />
     <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
