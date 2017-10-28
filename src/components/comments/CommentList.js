610fbc86b32a0ea2552ca48f13cfdf6f1aaa87d6
peteperson <peteperson_2000@yahoo.com>
2017-10-28 20:47:24 +0300

    add bootstrap table

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,9 +1,13 @@
 import React, { PropTypes } from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 const CommentList = ({ comments }) => {
-    console.log(comments.length);
     return (
-        <div>FUCK YOU !!!!</div>
+        <BootstrapTable data={comments}>
+            <TableHeaderColumn isKey dataField='id'>Comment ID</TableHeaderColumn>
+            <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
+            <TableHeaderColumn dataField='body'>Message</TableHeaderColumn>
+        </BootstrapTable>
     );
 };
 
